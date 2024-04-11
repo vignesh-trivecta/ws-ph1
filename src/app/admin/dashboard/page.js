ea1d6e5d5dbd70e83b651e00d74e7ed51775a0af
vignesh-trivecta <vignesh.murugan@trivectadigital.com>
2024-04-11 11:19:50 +0530

    update: new dashboard iframe url changed

--- src/app/admin/dashboard/page.js
+++ src/app/admin/dashboard/page.js
@@ -15,7 +15,7 @@ const DashCards = () => {
 
     return(
         <div className='container mx-auto mt-2' style={{width: '95%'}}>
-            <iframe class="w-full h-[calc(100vh-150px)]" frameborder="0" allowfullscreen src="https://analytics.zoho.in/open-view/333577000000175355"></iframe>
+            <iframe class="w-full h-[calc(100vh-150px)]" frameborder="0" allowfullscreen src="https://analytics.zoho.in/open-view/333577000000175355/50f05197c6efd369474c87ad15c3cb98"></iframe>
             {/* <iframe class="w-full h-[calc(100vh-150px)]" src="https://analytics.zoho.in/open-view/298633000000325786/bea6e30a1d1bc77ab8bfcf877648dd97" frameborder="0" allowfullscreen></iframe> */}
         </div>
     )
